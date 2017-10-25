3b1d4f9f2c447bc25ccac89b20a3c26f6c121cc9
Reigo Männiste <reigo.manniste@gmail.com>
2017-10-25 17:04:49 +0300

    Practice 4

---
+++ src/practice4/Exercise1.java
@@ -0,0 +1,21 @@
+package practice4;
+
+public class Exercise1 {
+
+    // Print out numbers on one line from 10 to 1
+
+    public static void main(String[] args) {
+
+        // for loop
+
+        // think about where to start the loop
+
+        // which way to decrease the integer in every cycle
+
+        for (int i = 10; i > 0; i--){
+
+            System.out.print(i + " ");
+
+        }
+    }
+}
